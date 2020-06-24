e01f184230a078c152c9de20724f32e1e606eb70
Mountech <mountmusic@yandex.ru>
2020-06-24 10:26:14 +0300

    Car

---
+++ vueLessons/src/Car.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<h3>Nm: {{ carName }}</h3>
+		<p>Yar:  {{carYear}} </p>
+	</div>
+</template>
+
+<script>
+export default {
+	data(){
+		return{
+			carName: 'Ford',
+			carYear: '2015'
+		}
+	}
+}
+</script>
