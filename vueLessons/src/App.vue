dadf774e687cb6eb71d04a81b62e70f0dc6a67b8
Mountech <mountmusic@yandex.ru>
2020-07-09 10:02:42 +0300

    Vue routes 5 fin

--- vueLessons/src/App.vue
+++ vueLessons/src/App.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="container">
+    <ul class="nav nav-tabs">
+      <!--Это способ дать Active класс на ссылку если это нужно родителю -->
+      <!-- <router-link tag="li" class="nav-item" exact to="/" active-class="active">
+        <a class="nav-link">Home</a>
+      </router-link> -->
+
+      <li class="nav-item">
+        <router-link class="nav-link" exact  active-class="active" :to="'/'">Home</router-link>
+      </li>
+
+      <li class="nav-item">
+        <router-link class="nav-link" exact  active-class="active" :to="'/cars'">Cars</router-link>
+      </li>
+
+      <li class="nav-item">
+        <router-link class="nav-link" exact  active-class="active" :to="'/car/3'">Cars 3</router-link>
+      </li>
+
+      <li class="nav-item">
+        <router-link class="nav-link" exact  active-class="active" :to="'/car/4'">Cars 4</router-link>
+      </li>
+    </ul>
     <router-view></router-view>
   </div> 
 </template>
---
+++ vueLessons/src/pages/Car.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<h1>Cars id {{ id }}</h1>
+	
+	</div>
+</template>
+
+<script>
+	export default{
+		data(){
+			return{
+				id: this.$router.currentRoute.params['id']
+			}
+		},
+		watch:{
+			$route (toR, FromR) {
+				this.id = toR.params['id']
+			}
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
