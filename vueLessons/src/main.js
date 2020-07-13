d670848f00f736b13e874498ed5ac0fe27ecf833
Mountech <mountmusic@yandex.ru>
2020-07-13 08:00:29 +0300

    Vuex install

--- vueLessons/src/main.js
+++ vueLessons/src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import App from './App.vue'
+import store from './store'
 
 Vue.use(VueResource)
 
 new Vue({ 
   el: '#app',
+  store,
   render: h => h(App),
 })
 
